117c336cd1f3e99e6599deea5243fdb3dc712f2f
Sally MacNicholas <smacnicholas@gmail.com>
2015-09-15 23:48:57 -0600

    add email index to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904175052) do
+ActiveRecord::Schema.define(version: 20150916054746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 20150904175052) do
     t.integer "role",            default: 0
     t.integer "purse",           default: 0
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
 
   add_foreign_key "loan_requests", "users"
 end
